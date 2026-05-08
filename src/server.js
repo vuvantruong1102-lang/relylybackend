43a5d10cb874287ae147f890db71881b95fe7df5
vuvantruong1102-lang <vuvantruong.1102@gmail.com>
2026-05-08 20:45:40 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
+// CORS - Cho phép Vercel gọi
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 app.use(express.json());
 
 // ===== CORS: Cho phép Vercel gọi Railway =====
